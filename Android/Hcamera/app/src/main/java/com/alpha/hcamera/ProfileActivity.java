1fa87213264f34401fcdbe9a747f2fe4aec04d78
Hyeoncho <iijokv@naver.com>
2019-09-05 13:40:23 +0900

    new camera

---
+++ Android/Hcamera/app/src/main/java/com/alpha/hcamera/ProfileActivity.java
@@ -0,0 +1,29 @@
+package com.alpha.hcamera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_layout);
+    }
+
+    private void setImage() {
+
+        ImageView imageView = findViewById(R.id.get_imageview);
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        Bitmap originalBm = BitmapFactory.decodeFile(tempFile.getAbsolutePath(), options);
+
+        imageView.setImageBitmap(originalBm);
+
+
+    }
+}
