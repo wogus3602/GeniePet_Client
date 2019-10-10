24035c4d327820df4a679ddc72f99b0921ddcf91
Hyeoncho <iijokv@naver.com>
2019-10-10 19:46:09 +0900

    MainMenuActivity

---
+++ Android/MainMenuActivity/MainMenu/app/src/main/java/com/alpha/mainmenu/MainItemInfo.java
@@ -0,0 +1,17 @@
+package com.alpha.mainmenu;
+
+public class MainItemInfo {
+    public int drawableId;
+    public String price;
+    public String name;
+    public String all;
+
+
+
+    public MainItemInfo(int drawableId, String price, String name, String all){
+        this.drawableId = drawableId;
+        this.price = price;
+        this.name = name;
+        this.all = all;
+    }
+}
---
+++ Android/MainMenuActivity/MainMenu/app/src/main/java/com/alpha/mainmenu/RecyclerVerticalAdapter.java
@@ -0,0 +1,53 @@
+package com.alpha.mainmenu;
+
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+public class RecyclerVerticalAdapter extends RecyclerView.Adapter<RecyclerVerticalAdapter.VerticalViewHolder>  {
+    private ArrayList<ArrayList<MainItemInfo>> AllMainItemInfo;
+    private Context context;
+
+    public  RecyclerVerticalAdapter(Context context, ArrayList<ArrayList<MainItemInfo>> data)
+    {
+        this.context = context;
+        this.AllMainItemInfo = data;
+    }
+
+    public class VerticalViewHolder extends RecyclerView.ViewHolder{
+        protected RecyclerView recyclerView;
+
+        public VerticalViewHolder(View view)
+        {
+            super(view);
+
+            this.recyclerView = (RecyclerView)view.findViewById(R.id.recyclerViewVertical);
+        }
+    }
+    @NonNull
+    @Override
+    public VerticalViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recycler_vertical, null);
+        return new RecyclerVerticalAdapter.VerticalViewHolder(v);
+    }
+    @Override
+    public void onBindViewHolder(@NonNull VerticalViewHolder verticalViewHolder, int position) {
+        RecyclerHorizontalAdapter adapter = new RecyclerHorizontalAdapter(AllMainItemInfo.get(position));
+
+        verticalViewHolder.recyclerView.setHasFixedSize(true);
+        verticalViewHolder.recyclerView.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false));
+        verticalViewHolder.recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public int getItemCount() {
+        return AllMainItemInfo.size();
+    }
+}
