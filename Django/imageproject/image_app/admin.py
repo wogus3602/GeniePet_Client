7bb08cc18fd357dee3a31a1d85ee1d21c6232cd8
jaehyunpark <wogus3602@naver.com>
2019-10-13 03:56:55 +0900

    서버 폴더 삭제

--- Django/imageproject/image_app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from image_app.models import MyImage
-
-
-admin.site.register(MyImage)
--- Django/imageproject/image_app/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-
-class MyImage(models.Model):
-	model_pic = models.ImageField(upload_to = '', default = 'none/no-img.jpg')
