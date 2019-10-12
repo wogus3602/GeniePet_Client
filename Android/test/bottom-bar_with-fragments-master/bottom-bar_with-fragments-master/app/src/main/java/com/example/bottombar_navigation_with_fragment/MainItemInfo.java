33bf64046bdd1073b5596ade2b1f5bd447f94aaa
jaehyunpark <wogus3602@naver.com>
2019-10-13 03:48:15 +0900

    메인코드 테스트

---
+++ Android/test/bottom-bar_with-fragments-master/bottom-bar_with-fragments-master/app/src/main/java/com/example/bottombar_navigation_with_fragment/MainItemInfo.java
@@ -0,0 +1,17 @@
+package com.example.bottombar_navigation_with_fragment;
+
+public class MainItemInfo {
+    public int drawableId;
+    public String price;
+    public String name;
+    public String all;
+
+
+
+    public MainItemInfo(int drawableId, String price, String name, String all){
+        this.drawableId = drawableId;
+        this.price = price;
+        this.name = name;
+        this.all = all;
+    }
+}
